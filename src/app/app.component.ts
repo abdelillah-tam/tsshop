4d138c7fafff83b67488bc455eb55fe878a7c90e
unknown <atamossat@gmail.com>
2024-10-02 23:36:00 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import Backendless from 'backendless';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterLink, RouterLinkActive, RouterOutlet, MatToolbarModule, MatIconModule, CurrencyPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'TSshop';
+  constructor(private router: Router) {
+    Backendless
+      .initApp('D80EE3D7-77E9-4614-8823-4277DF4DFF55',
+        '8CC8EB92-58D3-4949-931B-73217384B47A');
+  }
+
+
+  navigateToLogin() {
+    this.router.navigate(['/login']);
+  }
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        title: 'Home',
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        title: 'Login',
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+    }
+];
