eb0c35ebcb5bbf4ac6cb4650945fd153d91455b4
unknown <atamossat@gmail.com>
2024-10-20 00:47:10 +0100

    Changed to hash location

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
@@ -11,7 +11,7 @@ import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }),
-  provideRouter(routes),
+  provideRouter(routes, withHashLocation()),
   provideAnimationsAsync(),
   provideStore(),
   provideState({ name: 'user', reducer: userReducer }),
