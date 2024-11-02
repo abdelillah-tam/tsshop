526397782aca5e763914b31747a7998cda6ae491
unknown <atamossat@gmail.com>
2024-11-03 00:48:38 +0100

    Edits on Cart Component

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -2,30 +2,43 @@ import { Component, OnInit } from '@angular/core';
 import { MatStepperModule } from '@angular/material/stepper';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { Product } from '../model/product';
 import { getProductsFromCartAction } from '../store/actions';
-import { cartProductsSelector } from '../store/selectors';
+import { cartProductsSelector, userSelector } from '../store/selectors';
 import { CurrencyPipe } from '@angular/common';
+import { Router } from '@angular/router';
 
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [MatStepperModule, MatFormFieldModule, MatIconModule, ReactiveFormsModule, CurrencyPipe],
+  imports: [ReactiveFormsModule, MatStepperModule, MatFormFieldModule, MatIconModule, ReactiveFormsModule, CurrencyPipe],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.scss'
 })
 export class CartComponent implements OnInit {
 
-  constructor(private store: Store<Product[]>) { }
+  constructor(private router: Router, private store: Store<Product[]>) { }
 
   products: Product[] = [];
   total: number = 0;
 
+  infosGroup = new FormGroup({
+    fullName: new FormControl('', [Validators.required]),
+    address: new FormControl('', [Validators.required])
+  });
+
   ngOnInit(): void {
     this.store.dispatch(getProductsFromCartAction());
+    this.store.select(userSelector).subscribe((result) => {
+      if (result.valid && localStorage.length === 4) {
+      }
+      else {
+        this.router.navigate(['/home']);
+      }
+    });
     this.store.select(cartProductsSelector).subscribe((result) => {
       this.products = result;
       this.setTotal();
